2650d63aea5ca41355c662e2e60f60a5f58b8ed4
kadu-turing <kai.du@turing.com>
2023-07-20 18:59:32 +0000

    relax max token

--- backend/models/chats.py
+++ backend/models/chats.py
@@ -10,7 +10,7 @@ class ChatMessage(BaseModel):
     # A list of tuples where each tuple is (speaker, text)
     history: List[Tuple[str, str]]
     temperature: float = 0.0
-    max_tokens: int = 256
+    max_tokens: int = 1024
     use_summarization: bool = False
     chat_id: Optional[UUID] = None
     chat_name: Optional[str] = None
@@ -20,7 +20,7 @@ class ChatQuestion(BaseModel):
     model: str = "gpt-3.5-turbo"
     question: str
     temperature: float = 0.0
-    max_tokens: int = 256
+    max_tokens: int = 1024
 
 class Message(BaseModel):
     role: str
@@ -30,6 +30,6 @@ class ChatQuestionWithHistory(BaseModel):
     model: str = "gpt-3.5-turbo"
     question: str
     temperature: float = 0.0
-    max_tokens: int = 256
+    max_tokens: int = 1024
     history: List[Message] = []
     chat_id: Optional[UUID] = None
